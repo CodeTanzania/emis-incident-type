e63a8a648a506954a68588e25a6e66b572ef0f60
lykmapipo <lallyelias87@gmail.com>
2018-11-04 12:47:25 +0300

    Improve incident type model unit test

--- test/unit/incidenttype.model.spec.js
+++ test/unit/incidenttype.model.spec.js
@@ -42,12 +42,17 @@ describe('IncidentType Instance', () => {
 
 describe('IncidentType Statics', () => {
 
-  it('should expose model name as constant', () => {
+  it('should expose model name', () => {
     expect(IncidentType.MODEL_NAME).to.exist;
     expect(IncidentType.MODEL_NAME).to.be.equal('IncidentType');
   });
 
-  it('should expose autopulate as options', () => {
+  it('should expose collection name', () => {
+    expect(IncidentType.COLLECTION_NAME).to.exist;
+    expect(IncidentType.COLLECTION_NAME).to.be.equal('incidenttypes');
+  });
+
+  it('should expose autopulate options', () => {
     expect(IncidentType.OPTION_AUTOPOPULATE).to.exist;
     expect(IncidentType.OPTION_AUTOPOPULATE)
       .to.be.eql({
@@ -61,6 +66,51 @@ describe('IncidentType Statics', () => {
         },
         maxDepth: IncidentType.POPULATION_MAX_DEPTH
       });
+  });
+
+  it('should expose natures', () => {
+    expect(IncidentType.NATURE_OTHER).to.exist;
+    expect(IncidentType.NATURE_OTHER).to.be.equal('Other');
+
+    expect(IncidentType.NATURE_NATURAL).to.exist;
+    expect(IncidentType.NATURE_NATURAL).to.be.equal('Natural');
+
+    expect(IncidentType.NATURE_TECHNOLOGICAL).to.exist;
+    expect(IncidentType.NATURE_TECHNOLOGICAL).to.be.equal(
+      'Technological');
+
+    expect(IncidentType.NATURES).to.exist;
+    expect(IncidentType.NATURES).to.be.eql([
+      'Natural', 'Technological', 'Other'
+    ]);
+  });
+
+  it('should expose families', () => {
+    expect(IncidentType.FAMILY_OTHER).to.exist;
+    expect(IncidentType.FAMILY_OTHER).to.be.equal('Other');
+
+    expect(IncidentType.FAMILIES).to.exist;
+    expect(IncidentType.FAMILIES).to.be.eql([
+      'Geophysical', 'Meteorological',
+      'Hydrological', 'Climatological',
+      'Biological', 'Extra-terrestrial',
+      'Technological', 'Other'
+    ]);
+  });
+
+  it('should expose cap categories', () => {
+    expect(IncidentType.CAP_CATEGORY_OTHER).to.exist;
+    expect(IncidentType.CAP_CATEGORY_OTHER).to.be.equal('Other');
+
+    expect(IncidentType.CAP_CATEGORIES).to.exist;
+    expect(IncidentType.CAP_CATEGORIES).to.be.eql([
+      'Geo', 'Met',
+      'Safety', 'Security',
+      'Rescue', 'Fire',
+      'Health', 'Env',
+      'Transport', 'Infra',
+      'CBRNE', 'Other'
+    ]);
   });
 
 });
