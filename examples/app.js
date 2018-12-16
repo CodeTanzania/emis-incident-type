cb5256efc467d08936160266ad13f0d180ee2af3
lykmapipo <lallyelias87@gmail.com>
2018-12-17 02:26:02 +0300

    Improve example app

--- examples/app.js
+++ examples/app.js
@@ -1,39 +1,36 @@
 'use strict';
 
 
-/* ensure mongo uri */
+/* ensure mongodb uri */
 process.env.MONGODB_URI =
-  (process.env.MONGODB_URI || 'mongodb://localhost/emis-incident-type');
+  (process.env.MONGODB_URI || 'mongodb://localhost/emis-incidenttype');
 
 
 /* dependencies */
 const path = require('path');
-const mongoose = require('mongoose');
-const {
-  IncidentType,
-  apiVersion,
-  info,
-  app
-} = require(path.join(__dirname, '..'));
+const async = require('async');
+const { include } = require('@lykmapipo/include');
+const { connect } = require('@lykmapipo/mongoose-common');
+const { IncidentType, info, app } = include(__dirname, '..');
 
 
-/* connect to mongoose */
-mongoose.connect(process.env.MONGODB_URI);
+// establish mongodb connection
+connect((error) => {
 
+  // seed incident types
+  IncidentType.seed((error, results) => {
 
-IncidentType.seed(( /*error, results*/ ) => {
+    // expose module info
+    app.get('/', (request, response) => {
+      response.status(200);
+      response.json(info);
+    });
 
-  /* expose module info */
-  app.get('/', (request, response) => {
-    response.status(200);
-    response.json(info);
-  });
+    // fire the app
+    app.start((error, env) => {
+      console.log(`visit http://0.0.0.0:${env.PORT}`);
+    });
 
-  /* fire the app */
-  app.start((error, env) => {
-    console.log(
-      `visit http://0.0.0.0:${env.PORT}/v${apiVersion}/incidenttypes`
-    );
   });
 
 });
